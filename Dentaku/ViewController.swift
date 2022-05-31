f49afa7f29a72e5fb79fe08adf02ac20df718fd3
masaharu <dabao456democebu3@gmail.com>
2022-05-31 22:55:35 +0900

    レイアウトを作成した

--- Dentaku/ViewController.swift
+++ Dentaku/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resultLabel.layer.borderWidth = 3.0
+        resultLabel.layer.borderColor = UIColor.black.cgColor
     }
 
-
+    // MARK: Button
+    @IBAction func cancelButton(_ sender: Any) {
+    }
+    
+    @IBAction func resultButton(_ sender: Any) {
+    }
+    
+    @IBAction func plusButton(_ sender: Any) {
+    }
+    
+    @IBAction func minusButton(_ sender: Any) {
+    }
+    @IBAction func timesButton(_ sender: Any) {
+    }
+    
+    @IBAction func divideButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func pointButton(_ sender: Any) {
+    }
+    
+    @IBAction func one(_ sender: Any) {
+    }
+    
+    @IBAction func two(_ sender: Any) {
+    }
+    
+    @IBAction func three(_ sender: Any) {
+    }
+    
+    @IBAction func four(_ sender: Any) {
+    }
+    
+    @IBAction func five(_ sender: Any) {
+    }
+    @IBAction func six(_ sender: Any) {
+    }
+    @IBAction func seven(_ sender: Any) {
+    }
+    
+    @IBAction func eight(_ sender: Any) {
+    }
+    
+    @IBAction func nine(_ sender: Any) {
+    }
 }
 
