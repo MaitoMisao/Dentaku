d341839f2a6865d23aec68f9f4774ce3c12fece9
Shuji OCHI(mac mini) <ponpoko1968@gmail.com>
2022-06-16 10:53:10 +0900

    変数名とコメントを修正

--- Dentaku/CalcEngine.swift
+++ Dentaku/CalcEngine.swift
@@ -16,29 +16,29 @@ class CalcEngine {
         case mul
         // 本当は、桁あふれ、0除算などを考慮して.error状態が必要
     }
-    var result:String {
+    var displayedResult:String {
         get{
-            return _result
+            return _displayedResult
         }
         set {
             callBack?( newValue)
-            _result = newValue
+            _displayedResult = newValue
 
         }
     }
-    var register: String = "0" // ユーザの入力と＝ボタンで確定した計算結果を保存
-    var _result: String = "0" // これまでの計算結果を保存
+    var internalRegister: String = "0"  // これまでの計算結果を保存
+    var _displayedResult: String = "0" // ユーザの入力と＝ボタンで確定した計算結果を保存
     var mode :Mode = .none
     var callBack : ((String)->Void)? = nil
     func number(input:Int){ //
         // UIで制限される前提なので、0〜9以外の整数が入る場合は考慮しない。呼び出す側の責任で入力値をチェックすること
         assert(input >= 0 && input < 10, "UIで制限される前提なので、0〜9以外の整数が入る場合は考慮しない")
 
-        var registerVal = Decimal(string:register)! // 変換に失敗する場合は今回は考慮しない
+        var registerVal = Decimal(string:internalRegister)! // 変換に失敗する場合は今回は考慮しない
         let inputVal = Decimal(input)
         registerVal *= Decimal(10)
         registerVal += inputVal
-        register = "\(registerVal)"
+        internalRegister = "\(registerVal)"
     }
 
     func plus(){
@@ -46,16 +46,16 @@ class CalcEngine {
             return
         }
         mode = .plus
-        result = register
-        register = "0"
+        displayedResult = internalRegister
+        internalRegister = "0"
     }
 
     func equal(){
         switch mode {
         case .plus:
-            let resultVal = Decimal(string:_result)!
-            result = "\(resultVal + Decimal(string:register)!)"
-            register = "0"
+            let resultVal = Decimal(string:_displayedResult)!
+            displayedResult = "\(resultVal + Decimal(string:internalRegister)!)"
+            internalRegister = "0"
             mode = .none
 
         default:
