d4120a42c5bd900d1915527488bb121de616992b
Shuji OCHI(mac mini) <ponpoko1968@gmail.com>
2022-06-16 10:54:47 +0900

    ファイル名、ターゲット名を修正

--- DentakuTest/DentakuTest.swift
+++ DentakuTest/DentakuTest.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import Dentaku
 
-class com_mina_Dentaku: XCTestCase {
+class DentakuTest: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
